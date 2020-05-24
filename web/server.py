eb3d9a76ab65757024cdaddb412dcb9f2b784d72
yumilr <julisa.lapa@utec.edu.pe>
2020-05-23 22:21:11 -0500

    first try

--- web/server.py
+++ web/server.py
@@ -15,7 +15,146 @@ def static_content(content):
     return render_template(content)
 
 
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
+@app.route('/authenticate', methods=['POST'])
+def authenticate():
+    msg = json.loads(request.data)
+    #dummy validation
+    if msg['username'] == msg['password'] :
+        r_msg = {'msg':'Welcome'}
+        json_msg = json.dumps(r_msg)
+        return Response(json_msg, status=200, mimetype='application/json');
+    r_msg = {'msg':'Failed'}
+    json_msg = json.dumps(r_msg)
+    return Response(json_msg, status=401, mimetype='application/json')
+
+#CRUD users
+@app.route('/users', methods = ['POST'])
+def create_user():
+    #c = json.loads(request.data)
+    c = json.loads(request.form['values'])
+    user = entities.User(
+        username=c['username'],
+        name=c['name'],
+        fullname=c['fullname'],
+        password=c['password']
+    )
+    session = db.getSession(engine)
+    session.add(user)
+    session.commit()
+    r_msg = {'msg':'UserCreated'}
+    json_msg = json.dumps(r_msg)
+    return Response(json_msg, status=201)
+
+@app.route('/users/<id>', methods = ['GET'])
+def get_user(id):
+    db_session = db.getSession(engine)
+    users = db_session.query(entities.User).filter(entities.User.id == id)
+    for user in users:
+        js = json.dumps(user, cls=connector.AlchemyEncoder)
+        return  Response(js, status=200, mimetype='application/json')
+    message = { 'status': 404, 'message': 'Not Found'}
+    return Response(json.dumps(message), status=404, mimetype='application/json')
+
+
+@app.route('/users', methods = ['GET'])
+def get_users():
+    session = db.getSession(engine)
+    dbResponse = session.query(entities.User)
+    data = dbResponse[:]
+    return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype='application/json')
+
+
+@app.route('/users', methods = ['PUT'])
+def update_user():
+    session = db.getSession(engine)
+    id = request.form['key']
+    user = session.query(entities.User).filter(entities.User.id == id).first()
+    c = json.loads(request.form['values'])
+
+    for key in c.keys():
+        setattr(user, key, c[key])
+
+    session.add(user)
+    session.commit()
+    return 'Updated User'
+
+
+@app.route('/users', methods = ['DELETE'])
+def delete_user():
+    id = request.form['key']
+    session = db.getSession(engine)
+    user = session.query(entities.User).filter(entities.User.id == id).one()
+    session.delete(user)
+    session.commit()
+    return "Deleted User"
+
+
+@app.route('/current', methods = ['GET'])
+def current_user():
+    db_session = db.getSession(engine)
+    user = db_session.query(entities.User).filter(entities.User.id == session['logged_user']).first()
+    return Response(json.dumps(user, cls = connector.AlchemyEncoder), mimetype = 'application/json')
+
+#CRUD messages
+@app.route('/message', methods = ['POST'])
+def create_message():
+    c = json.loads(request.form['values'])
+    message = entities.Message(
+        content=c['content'],
+        sent_on=c['sent_on'],
+        user_from_id=c['user_from_id'],
+        user_to_id=c['user_to_id']
+    )
+    session = db.getSession(engine)
+    session.add(user)
+    session.commit()
+    r_msg = {'msg':'Message Created!'}
+    json_msg = json.dumps(r_msg)
+    return Response(json_msg, status=201)
+
+@app.route('/messages/<id>', methods = ['GET'])
+def get_message(id):
+    db_session = db.getSession(engine)
+    messages = db_session.query(entities.Message).filter(entities.Message.id == id)
+    for user in messages:
+        js = json.dumps(user, cls=connector.AlchemyEncoder)
+        return  Response(js, status=200, mimetype='application/json')
+    message = { 'status': 404, 'message': 'Not Found'}
+    return Response(json.dumps(message), status=404, mimetype='application/json')
+
+@app.route('/messages', methods = ['GET'])
+def get_messages():
+    session = db.getSession(engine)
+    dbResponse = session.query(entities.Message)
+    data = dbResponse[:]
+    return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype='application/json')
+
+@app.route('/messages', methods = ['PUT'])
+def update_message():
+    session = db.getSession(engine)
+    id = request.form['key']
+    user = session.query(entities.User).filter(entities.Message.id == id).first()
+    c = json.loads(request.form['values'])
+
+    for key in c.keys():
+        setattr(message, key, c[key])
+
+    session.add(message)
+    session.commit()
+    return 'Updated Message!'
+
+@app.route('/messages', methods = ['DELETE'])
+def delete_message():
+    id = request.form['key']
+    session = db.getSession(engine)
+    message = session.query(entities.Message).filter(entities.Message.id == id).one()
+    session.delete(message)
+    session.commit()
+    return "Deleted Message!"
 
 if __name__ == '__main__':
     app.secret_key = ".."
---
+++ web/static/js/messages.js
@@ -0,0 +1,44 @@
+$(function(){
+    var url = '/messages'
+    $("#grid").dxDataGrid({
+        dataSource: DevExpress.data.AspNet.createStore({
+            key: "id",
+            loadUrl: url,
+            insertUrl: url,
+            updateUrl: url,
+            deleteUrl: url,
+            onBeforeSend: function(method, ajaxOptions) {
+                ajaxOptions.xhrFields = { withCredentials: true };
+            }
+        }),
+
+        editing: {
+            allowUpdating: true,
+            allowDeleting: true,
+            allowAdding: true
+        },
+
+        paging: {
+            pageSize: 12
+        },
+
+        pager: {
+            showPageSizeSelector: false,
+            allowedPageSizes: [8, 12, 20]
+        },
+
+        columns: [{
+            dataField: "id",
+            dataType: "number",
+            allowEditing: false
+        }, {
+            dataField: "Content"
+        }, {
+            dataField: "Sent on"
+        }, {
+            dataField: "User from id"
+        }, {
+            dataField: "User to id"
+        }, ],
+    }).dxDataGrid("instance");
+});
